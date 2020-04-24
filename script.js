c566ba5117992288fe253a3d70d21aff646f3b76
Paulina Weiher <45978460+theCieniu@users.noreply.github.com>
2020-04-24 13:54:16 +0200

    data

--- script.js
+++ script.js
@@ -1,1 +1,58 @@
-document.addEventListener("scroll", alert("first test"))
+// MY EYESHADOW COLLECTION
+// IN THE FUTURE - user input
+
+let myCollection = {
+  palette1: {
+    name: "Sweet Peach",
+    eyeshadows: ["White Peach", "Luscious", "Just Peachy", "Bless Her Heart", "Tempting", "Charmed, I'm Sure", "Nectar", "Cobbler", "Candied Peach", "Bellini", "Peach Pit", "Delectable", "Peaches 'N Cream", "Georgia", "Carmelized", "Puree", "Summer Yum", "Talk Derby To Me"]
+  },
+  palette2: {
+    name: "Cat Eyes",
+    eyeshadows: ["Cat Eyes": ["Purr", "Meow", "Pussy Cat", "Jungle Love", "Kitten", "Kitty Glitter - green", "Panther"]]
+  },
+  palette3: {
+    name: "Meet Matt(e)",
+    eyeshadows: ["Johnson", "Garcia", "Malloy", "Rosen", "Wood", "Singh", "Abdul", "Lombardi", "Hung"]
+  },
+  palette4: {
+    name: "Affinity",
+    eyeshadows: ["1-Nawab", "2-Hussain", "3-Aftab", "4-Munir", "5-Naveed", "6-Fareed", "7-Ijaz", "8-Seemab", "9-Aqib", "10-Nadim", "11-Alber", "12-Shiraz", "13-Shmail", "14-Fraz", "15-Bilal", "16-Shazad", "17-Shuhaib", "18-Shafay"]
+  },
+  palette5: {
+    name: "Dreamy",
+    eyeshadows: ["Immaculate", "Illusion", "Vanitas", "Delirium", "Bryzantine", "Sistina", "Metal Cupid", "Inception", "Senorita", "Rose Gold", "Lullaby", "Dogma"]
+  },
+  palette6: {
+    name: "Zulu",
+    eyeshadows: ["1-Orange", "2-Yellow", "3-Brown", "4-Turquoise", "5-Lime Green", "6-Blue", "7-Rose Gold", "8-Pink", "9-Purple"]
+  },
+  palette7: {
+    name: "Galaxy Chic",
+    eyeshadows: ["Sun", "Jupiter", "Prometheus", "Aphrodite", "Milky Way", "Cosmic", "Saturn", "Venus", "Meteor", "Comet", "Earth", "Neptune", "Mercury", "Mars", "Asteroid", "Electra", "Moon", "Pluto"]
+  },
+  palette8: {
+    name: "Glam Shop",
+    eyeshadows: ["Disco", "Rewia", "Elektryczna Pomarańcza", "Wata Cukrowa", "Lala", "Satelita", "Ornament", "Czerwony Wirus", "Puszek", "Kokietka", "Perskie Oko", "Zielony Amber", "Swietlik", "Błyskotka", "Pył Księżycowy", "Jednorożec", "Balanga", "Wirus", "Cytrynowa Mgiełka", "Holo", "Zorza", "Rio", "Fioletowe holo brokat", "Krem brokatowy", "Alarm", "Szefuńcio", "świecki", "Festiwalowy", "Balonówka", "Nie święty", "Królowa pszczół", "Fioletowy róż", "Sasanka", "Czeko Czeko", "Sople", "Ładny szary"]
+  },
+  palette9: {
+    name: "Singleshadow",
+    eyeshadows: ["Inglot 301", "Sleek Marsala", "Sleek Brick", "Sleek Yellow Gold", "Sleek Pink Purple", "Sleek Black Shimmer", "Silver", "Sleek Warm green", "Sleek Cold Green", "Sephora Tiramisu", "My Secret Dark Green", "Sensique Dark Blue", "My Secret Orchid Pink", "My Secret Purple", "My Secret Lime Green", "My Secret purple blue Seduce", "Rillakuma", "Nyx Holo glitter", "Essence glitter", "Essence liquid gold", "Eveline liquid platinym", "Catrice Satina van der Woodsen", "My Secret gold pigment"]
+  },
+  palette10: {
+    name: "Mineral",
+    eyeshadows: ["AM Vanilla", "AM Chocolate", "AM Almond Milk", "AM Cocoa Cup", "AM Iced tea", "AM Lilac", "AM Cappucino", "AM Milkshake", "AM Smoothie", "AM Lemonade", "Neauty Volcanic Ash", "Neauty Stormy Cloud", "Duochrome"]
+  }
+}
+
+
+// CREATING EYESHADOW LIST
+
+
+let poll = [];
+//for each?
+
+
+
+ //
+
+let eyeshadowNumber = // poll length
