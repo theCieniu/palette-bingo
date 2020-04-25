b5e7382c25390f4fdee472385500743b98e67a2a
Paulina Weiher <45978460+theCieniu@users.noreply.github.com>
2020-04-25 13:27:31 +0200

    pick random
    Pick random eyeshadow button and function; result in console

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ let myCollection = {
   },
   palette2: {
     name: "Cat Eyes",
-    eyeshadows: ["Cat Eyes": ["Purr", "Meow", "Pussy Cat", "Jungle Love", "Kitten", "Kitty Glitter - green", "Panther"]]
+    eyeshadows: ["Purr", "Meow", "Pussy Cat", "Jungle Love", "Kitten", "Kitty Glitter - green", "Panther"]
   },
   palette3: {
     name: "Meet Matt(e)",
-    eyeshadows: ["Johnson", "Garcia", "Malloy", "Rosen", "Wood", "Singh", "Abdul", "Lombardi", "Hung"]
+    eyeshadows: ["Matt Johnson", "Matt Garcia", "Matt Malloy", "Matt Rosen", "Matt Wood", "Matt Singh", "Matt Abdul", "Matt Lombardi", "Matt Hung"]
   },
   palette4: {
     name: "Affinity",
@@ -40,7 +40,7 @@ let myCollection = {
   },
   palette10: {
     name: "Mineral",
-    eyeshadows: ["AM Vanilla", "AM Chocolate", "AM Almond Milk", "AM Cocoa Cup", "AM Iced tea", "AM Lilac", "AM Cappucino", "AM Milkshake", "AM Smoothie", "AM Lemonade", "Neauty Volcanic Ash", "Neauty Stormy Cloud", "Duochrome"]
+    eyeshadows: ["AM Vanilla", "AM Chocolate", "AM Almond Milk", "AM Cocoa Cup", "AM Iced tea", "AM Lilac", "AM Cappucino", "AM Milkshake", "AM Smoothie", "AM Lemonade", "Neauty Volcanic Ash", "Neauty Stormy Cloud", "Mineral Duochrome"]
   }
 }
 
@@ -51,8 +51,26 @@ let myCollection = {
 let poll = [];
 //for each?
 
+//HARD CODING - TO BE DELETED
 
-
+poll = ["White Peach", "Luscious", "Just Peachy", "Bless Her Heart", "Tempting", "Charmed, I'm Sure", "Nectar", "Cobbler", "Candied Peach", "Bellini", "Peach Pit", "Delectable", "Peaches 'N Cream", "Georgia", "Carmelized", "Puree", "Summer Yum", "Talk Derby To Me", "Purr", "Meow", "Pussy Cat", "Jungle Love", "Kitten", "Kitty Glitter - green", "Panther", "Matt Johnson", "Matt Garcia", "Matt Malloy", "Matt Rosen", "Matt Wood", "Matt Singh", "Matt Abdul", "Matt Lombardi", "Matt Hung", "1-Nawab", "2-Hussain", "3-Aftab", "4-Munir", "5-Naveed", "6-Fareed", "7-Ijaz", "8-Seemab", "9-Aqib", "10-Nadim", "11-Alber", "12-Shiraz", "13-Shmail", "14-Fraz", "15-Bilal", "16-Shazad", "17-Shuhaib", "18-Shafay", "Immaculate", "Illusion", "Vanitas", "Delirium", "Bryzantine", "Sistina", "Metal Cupid", "Inception", "Senorita", "Rose Gold", "Lullaby", "Dogma", "1-Orange", "2-Yellow", "3-Brown", "4-Turquoise", "5-Lime Green", "6-Blue", "7-Rose Gold", "8-Pink", "9-Purple", "Sun", "Jupiter", "Prometheus", "Aphrodite", "Milky Way", "Cosmic", "Saturn", "Venus", "Meteor", "Comet", "Earth", "Neptune", "Mercury", "Mars", "Asteroid", "Electra", "Moon", "Pluto", "Disco", "Rewia", "Elektryczna Pomarańcza", "Wata Cukrowa", "Lala", "Satelita", "Ornament", "Czerwony Wirus", "Puszek", "Kokietka", "Perskie Oko", "Zielony Amber", "Swietlik", "Błyskotka", "Pył Księżycowy", "Jednorożec", "Balanga", "Wirus", "Cytrynowa Mgiełka", "Holo", "Zorza", "Rio", "Fioletowe holo brokat", "Krem brokatowy", "Alarm", "Szefuńcio", "świecki", "Festiwalowy", "Balonówka", "Nie święty", "Królowa pszczół", "Fioletowy róż", "Sasanka", "Czeko Czeko", "Sople", "Ładny szary", "Inglot 301", "Sleek Marsala", "Sleek Brick", "Sleek Yellow Gold", "Sleek Pink Purple", "Sleek Black Shimmer", "Silver", "Sleek Warm green", "Sleek Cold Green", "Sephora Tiramisu", "My Secret Dark Green", "Sensique Dark Blue", "My Secret Orchid Pink", "My Secret Purple", "My Secret Lime Green", "My Secret purple blue Seduce", "Rillakuma", "Nyx Holo glitter", "Essence glitter", "Essence liquid gold", "Eveline liquid platinym", "Catrice Satina van der Woodsen", "My Secret gold pigment", "AM Vanilla", "AM Chocolate", "AM Almond Milk", "AM Cocoa Cup", "AM Iced tea", "AM Lilac", "AM Cappucino", "AM Milkshake", "AM Smoothie", "AM Lemonade", "Neauty Volcanic Ash", "Neauty Stormy Cloud", "Mineral Duochrome"]
  //
 
-let eyeshadowNumber = // poll length
+let eyeshadowNumber = poll.length;
+console.log(eyeshadowNumber)
+let randomNumber
+let randomEyeshadow
+
+let button = document.getElementById("randomize")
+
+
+
+
+
+//pickEyeshadow()
+
+button.addEventListener("click", () => {
+  randomNumber = Math.floor(Math.random() * eyeshadowNumber);
+  console.log(randomNumber)
+  console.log(poll[randomNumber - 1])
+});
